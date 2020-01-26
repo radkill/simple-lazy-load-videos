521dcc1acc22aa67bec295abe42978f9e6b1216c
Valerii Bohdanov <valerii.bohdanov@ideus.biz>
2020-01-26 05:30:07 +0200

    Update description

--- simple-lazy-load-videos.php
+++ simple-lazy-load-videos.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:       Simple Lazy Load Videos
  * Plugin URI:        https://github.com/radkill/simple-lazy-load-videos
- * Description:       Simple Lazy Load Videos from Youtube & Vimeo
+ * Description:       Simple Lazy Load Videos for embeded Youtube & Vimeo
  * Version:           0.3.2
  * Requires PHP:      5.6
  * Author:            Valerii Bohdanov
