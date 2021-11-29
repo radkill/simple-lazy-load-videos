ee7ba3d531635c8b3bc7e8258cfc9fffa080b5f8
Valerii Bohdanov <valerii.bohdanov@ideus.biz>
2021-11-29 17:35:40 +0200

    Moved other functions to class SLLV_main

---
+++ includes/class-main.php
@@ -0,0 +1,120 @@
+<?php
+if ( ! class_exists( 'SLLV_main' ) ) {
+	class SLLV_main {
+
+		/**
+		 * Class initialization
+		 */
+		public function __construct() {
+			$this->check_version();
+
+			/** Register all hooks */
+			add_action( 'plugins_loaded', array( $this, 'load_textdomain' ) );
+			add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
+			add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_styles' ) );
+			add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_styles' ) );
+			add_action( 'after_setup_theme', array( $this, 'editor_style' ) );
+
+			add_filter( 'oembed_dataparse', array( $this, 'change_oembed' ), 10, 3 );
+			add_action( 'save_post', array( $this, 'flush_oembed_cache' ), 10, 3 );
+		}
+
+
+		/**
+		 * Load plugin textdomain
+		 */
+		function load_textdomain() {
+			load_plugin_textdomain( 'simple-lazy-load-videos', false, SLLV_PLUGIN_DIRNAME . '/languages/' );
+		}
+
+
+		/**
+		 * Enqueue JavaScripts
+		 */
+		public function enqueue_scripts() {
+			if ( file_exists( SLLV_PATH . 'assets/js/scripts.js' ) ) {
+				wp_enqueue_script( 'sllv-js-main', SLLV_URL . 'assets/js/scripts.js', array( 'jquery' ), filemtime( SLLV_PATH . 'assets/js/scripts.js' ), true );
+			}
+		}
+
+
+		/**
+		 * Enqueue CSS
+		 */
+		public function enqueue_styles() {
+			if ( file_exists( SLLV_PATH . 'assets/css/main.min.css' ) ) {
+				wp_enqueue_style( 'sllv-css-main', SLLV_URL . 'assets/css/main.min.css', false, filemtime( SLLV_PATH . 'assets/css/main.min.css' ) );
+			}
+		}
+
+
+		/**
+		 * Editor CSS
+		 */
+		public function editor_style() {
+			if ( file_exists( SLLV_PATH . 'assets/css/main.min.css' ) ) {
+				add_editor_style( SLLV_URL . 'assets/css/main.min.css' );
+			}
+		}
+
+
+		/**
+		 * Plugin version check & update
+		 */
+		public function check_version() {
+			$version = get_option( 'sllv_version' );
+
+			if ( ! $version || version_compare( $version, SLLV_VERSION, '!=' ) ) {
+				$oembed_cache = new SLLV_Oembed_Cache();
+				$oembed_cache->flush_all();
+
+				update_option( 'sllv_version', SLLV_VERSION );
+			}
+		}
+
+
+		/**
+		 * Change video oEmbed
+		 */
+		function change_oembed( $return, $data, $url ) {
+			$video = new SLLV_template();
+
+			if ( 'YouTube' === $data->provider_name ) {
+				preg_match( "/embed\/([-\w]+)\?feature=/", $data->html, $matches );
+				$video_id = $matches[1];
+
+				$return = $video->video( array(
+					'provider'  => 'youtube',
+					'title'     => $data->title,
+					'id'        => $video_id,
+					'url'       => 'https://youtu.be/' . $video_id,
+					'thumbnail' => 'https://i.ytimg.com/vi/' . $video_id . '/hqdefault.jpg',
+					'play'      => $video->youtube,
+				) );
+			}
+
+			if ( 'Vimeo' === $data->provider_name ) {
+				$return = $video->video( array(
+					'provider'  => 'vimeo',
+					'title'     => $data->title,
+					'id'        => $data->video_id,
+					'url'       => 'https://vimeo.com/' . $data->video_id,
+					'thumbnail' => $data->thumbnail_url,
+					'play'      => $video->vimeo,
+				) );
+			}
+
+			return $return;
+		}
+
+
+		/**
+		 * Flush oembed cache
+		 */
+		public function flush_oembed_cache( $post_ID, $post, $update ) {
+			$oembed_cache = new SLLV_Oembed_Cache();
+			$oembed_cache->flush( $post_ID );
+		}
+
+	}
+}
--- simple-lazy-load-videos.php
+++ simple-lazy-load-videos.php
@@ -18,8 +18,13 @@ defined( 'ABSPATH' ) or die( 'No script kiddies please!' ); // Exit if accessed 
 if ( ! defined( 'SLLV_URL' ) ) {
 	define( 'SLLV_URL', plugin_dir_url( __FILE__ ) );
 }
+
 if ( ! defined( 'SLLV_PATH' ) ) {
 	define( 'SLLV_PATH', plugin_dir_path( __FILE__ ) );
+}
+
+if ( ! defined( 'SLLV_PLUGIN_DIRNAME' ) ) {
+	define( 'SLLV_PLUGIN_DIRNAME', dirname( plugin_basename( __FILE__ ) ) );
 }
 
 if ( ! defined( 'SLLV_VERSION' ) ) {
@@ -28,14 +33,12 @@ if ( ! defined( 'SLLV_VERSION' ) ) {
 	define( 'SLLV_VERSION', $plugin_data['Version'] );
 }
 
-require_once( SLLV_PATH . 'includes/plugin-styles.php' );
-require_once( SLLV_PATH . 'includes/plugin-scripts.php' );
-
 
 /**
  * Classes
  */
 require_once( SLLV_PATH . 'includes/class-actions.php' );
+require_once( SLLV_PATH . 'includes/class-main.php' );
 require_once( SLLV_PATH . 'includes/class-oembed-cache.php' );
 require_once( SLLV_PATH . 'includes/class-template.php' );
 
@@ -50,79 +53,8 @@ register_uninstall_hook( __FILE__, array( $actions, 'uninstall' ) );
 
 
 /**
- * Plugin version check & update
+ * Start this plugin once all other plugins are fully loaded
  */
-if ( ! function_exists( 'sllv_check_version' ) ) {
-	function sllv_check_version() {
-		$version = get_option( 'sllv_version' );
-
-		if ( ! $version || version_compare( $version, SLLV_VERSION, '!=' ) ) {
-			$oembed_cache = new SLLV_Oembed_Cache();
-			$oembed_cache->flush_all();
-
-			update_option( 'sllv_version', SLLV_VERSION );
-		}
-	}
-}
-sllv_check_version();
-
-
-/**
- * Load plugin textdomain
- */
-if ( ! function_exists( 'sllv_init' ) ) {
-	add_action( 'plugins_loaded', 'sllv_init' );
-	function sllv_init() {
-		load_plugin_textdomain( 'simple-lazy-load-videos', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
-	}
-}
-
-
-/**
- * Flush oembed cache on post update
- */
-if ( ! function_exists( 'sllv_flush_oembed_cache' ) ) {
-	add_action( 'save_post', 'sllv_flush_oembed_cache', 10, 3 );
-	function sllv_flush_oembed_cache( $post_ID, $post, $update ) {
-		$oembed_cache = new SLLV_Oembed_Cache();
-		$oembed_cache->flush( $post_ID );
-	}
-}
-
-
-/**
- * Lazy Load Videos
- */
-if ( ! function_exists( 'sllv_change_oembed' ) ) {
-	add_filter( 'oembed_dataparse', 'sllv_change_oembed', 10, 3 );
-	function sllv_change_oembed( $return, $data, $url ) {
-		$video = new SLLV_template();
-
-		if ( 'YouTube' === $data->provider_name ) {
-			preg_match( "/embed\/([-\w]+)\?feature=/", $data->html, $matches );
-			$video_id = $matches[1];
-
-			$return = $video->video( array(
-				'provider'  => 'youtube',
-				'title'     => $data->title,
-				'id'        => $video_id,
-				'url'       => 'https://youtu.be/' . $video_id,
-				'thumbnail' => 'https://i.ytimg.com/vi/' . $video_id . '/hqdefault.jpg',
-				'play'      => $video->youtube,
-			) );
-		}
-
-		if ( 'Vimeo' === $data->provider_name ) {
-			$return = $video->video( array(
-				'provider'  => 'vimeo',
-				'title'     => $data->title,
-				'id'        => $data->video_id,
-				'url'       => 'https://vimeo.com/' . $data->video_id,
-				'thumbnail' => $data->thumbnail_url,
-				'play'      => $video->vimeo,
-			) );
-		}
-
-		return $return;
-	}
-}
+add_action( 'plugins_loaded', function() {
+	new SLLV_main();
+} );
