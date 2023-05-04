7a495cff75a7c82fa2cbe403fc7fe422954b64f5
Valerii Bohdanov <valerii.bohdanov@ideus.biz>
2023-05-04 18:26:47 +0300

    Update languages file

--- simple-lazy-load-videos.php
+++ simple-lazy-load-videos.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:       Simple Lazy Load Videos
  * Plugin URI:        https://github.com/radkill/simple-lazy-load-videos
- * Description:       Simple Lazy Load for embedded video from Youtube and Vimeo
+ * Description:       Simple Lazy Load for embedded video from Youtube and Vimeo.
  * Version:           1.0.0
  * Requires at least: 4.9
  * Requires PHP:      5.6
